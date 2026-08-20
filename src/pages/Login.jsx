bcf75b925219f771eab8f5702ec0c38e0da29af2
Shamxc <shamellemserrano@gmail.com>
2026-08-20 17:48:08 +0800

    login

---
+++ src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+function App(){
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    return(
+
+        <div>
+
+            <form>
+
+                <label>Username</label>
+                <input></input>
+
+                <label>Password</label>
+                <input type="password"></input>
+
+                <button type="submit">Login</button>
+
+            </form>
+
+        </div>
+    );
+}

7dfed76c3ce8af565ab88cdd44658cf7618f1f67
Shamxc <shamellemserrano@gmail.com>
2026-08-20 20:01:45 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import AttendanceChecker from './pages/AttendanceChecker'
+import ElectricityBill from './pages/ElectricityBill'
+import GradeEvaluation from './pages/GradeEvaluaton'
 import Login from './pages/Login'
+import PasswordChecker from './pages/PasswordChecker'
 
 function App() {
+	const [currentPage, setCurrentPage] = useState('login')
 
+	const pages = {
+		login: <Login />,
+		attendance: <AttendanceChecker />,
+		electricity: <ElectricityBill />,
+		grades: <GradeEvaluation />,
+		password: <PasswordChecker />,
+	}
 
-  return (
-    <>
-    <Login/>
+	return (
+		<>
+			<nav>
+				<button onClick={() => setCurrentPage('login')}>Login</button>
+				<button onClick={() => setCurrentPage('attendance')}>Attendance</button>
+				<button onClick={() => setCurrentPage('electricity')}>Electricity</button>
+				<button onClick={() => setCurrentPage('grades')}>Grades</button>
+				<button onClick={() => setCurrentPage('password')}>Password</button>
+			</nav>
 
-    </>
-  )
+			{pages[currentPage]}
+		</>
+	)
 }
 
 export default App
+
